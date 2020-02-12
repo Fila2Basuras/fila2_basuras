05b61654a83bc6f37ca189263d4b352397dbaeed
unknown <adriantroyasevilla>
2020-02-12 13:23:39 +0100

    Mostrar calendario del usuario logueado

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, request, url_for
 from libreria import *
+from bson import ObjectId
 import os
 
 from pymongo import MongoClient
@@ -10,12 +11,22 @@ MONGO_URL_ATLAS = 'mongodb+srv://admin:TGTnmlU06H27VEf0@cluster0-6nuch.mongodb.n
 client = MongoClient(MONGO_URL_ATLAS, ssl_cert_reqs=False)
 db = client['agenda_residuos']
 collection = db['calendario']
+collection_usuarios = db['usuarios']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    ejemplo = list(collection.find({}))
-    return render_template('index.html', ejemplo=ejemplo)
+    return render_template('index.html')
 
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if 'email' in session:
+        email = session["email"]
+        usuario = collection_usuarios.find_one({'email': email})
+        id = usuario["_id"]
+        calendario = collection.find_one({'usuario' : ObjectId(id)})
+        return render_template('home.html', usuario = calendario)
+    else:
+        return redirect(url_for('index'))
 
 if __name__ == "__main__":
     # Esto es un problema porque no le podemos poner un puerto de salida, para eso vamos a crear lo siguiente:

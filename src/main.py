6d80332aef58d9af17d6153fb25e112af568ea13
cechauri <echauricristian@gmail.com>
2020-02-12 13:23:29 +0100

    WIP: Formulario de configuracion del calendario. Falta: referencia a usuario y guardar en la db. Deseable: cargar formulario con la actual configuracion del usuario.

--- src/main.py
+++ src/main.py
@@ -17,6 +17,38 @@ def index():
     return render_template('index.html', ejemplo=ejemplo)
 
 
+@app.route('/calendario/create', methods=('GET', 'POST'))
+def createCalendario():
+    
+    context = {}
+    context['semana'] = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']
+    context['horario_recogida'] = ['mañana', 'tarde', 'noche']
+    context['opcion_recogida'] = ['cristal', 'organicos', 'papel', 'ninguno']
+    error = None
+
+    if request.method == 'POST':
+        calendario = {}
+
+        for value in context['semana']:
+            calendario[value] = {}
+
+            for horario in context['horario_recogida']:
+                
+                input_name = value + '_' + horario
+                calendario[value][horario] = request.form.get(input_name, '')
+                
+                if calendario[value][horario] not in context['opcion_recogida'] and calendario[value][horario] != '':
+                    error = 'Valor no permitido'
+                    break
+            if error:
+                break
+        
+        if not error:
+            #### GUARDAR EN DB
+            print(calendario)
+
+    return render_template('calendario/create.html', error=error, context=context)
+
 if __name__ == "__main__":
     # Esto es un problema porque no le podemos poner un puerto de salida, para eso vamos a crear lo siguiente:
     # SI HAY VARIABLE DE ENTORNO PORT COJE ESA VARIABLE, SI NO COJE EL 5000
